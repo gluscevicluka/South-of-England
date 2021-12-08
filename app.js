07bc2cd5d829b46b6bdf00d4edcea9ac260cd20e
gluscevicluka <gluscevicluka@gmail.com>
2021-12-08 14:22:21 +0100

    valjda ce biti dobro

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const read_more_content = document.querySelector("#read-more-content");
 read_more.addEventListener("click", function () {
   if ((read_more_content.style.display === "block")) {
     read_more_content.style.display = "none";
+    read_more.innerHTML = 'Read more';
   } else {
     read_more_content.style.display = "block";
+    read_more.innerHTML = 'Show less';
   }
 });
