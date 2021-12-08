a36d9b55a3da018f175f35183349853d92bd0286
gluscevicluka <gluscevicluka@gmail.com>
2021-12-08 15:26:39 +0100

    prototype project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const read_more = document.querySelector("#read_more");
 const read_more_content = document.querySelector("#read-more-content");
 
+
 read_more.addEventListener("click", function () {
   if ((read_more_content.style.display === "block")) {
     read_more_content.style.display = "none";
